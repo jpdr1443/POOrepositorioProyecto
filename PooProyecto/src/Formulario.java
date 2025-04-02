2048355eaa167306b610d9ce2b8c8ef181e95191
jpdr1443 <jpdr1443@gmail.com>
2025-04-02 13:06:31 -0500

    este es la primera vista de este proyecto

--- PooProyecto/src/Formulario.java
+++ PooProyecto/src/Formulario.java
@@ -1,4 +1,77 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
 
-public class Formulario {
+public class Formulario extends JPanel {
+    private  JLabel labelNombre, labelApellido, labelND, labelTipoDoc;
+    private TextField campoNombre, campoApellido, campoND;
+    private JComboBox tipoDocumento;
+
+    public Formulario(){
+        setLayout(null);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        labelTipoDoc = new JLabel("Tipo de documento:");
+        labelTipoDoc.setBounds(10, 20, 120, 40);
+        labelND = new JLabel("Numero de Documento:");
+        labelND.setBounds(10, 60, 140, 40);
+        labelNombre = new JLabel("Nombre:");
+        labelNombre.setBounds(10,100,50, 30);
+        labelApellido = new JLabel("Apellido:");
+        labelApellido.setBounds(10,140,50, 40);
+        //textFields y sus ajustes.
+        campoNombre = new TextField(20);
+        campoNombre.setBounds(70, 106,80,20);
+        campoApellido = new TextField(20);
+        campoApellido.setBounds(70,150,80, 20);
+        campoND = new TextField(20);
+        campoND.setBounds(150, 70, 80, 20);
+
+
+        //Lista de tipos de documento
+        String[] tipos = {"Cedula de Ciudadania", "Documento de identidad", "PEP", "Pasaporte", "Registro Civil"};
+        tipoDocumento = new JComboBox<>(tipos);
+        tipoDocumento.setBounds(130, 30, 150, 20);
+        add(labelTipoDoc);
+        add(labelND);
+        add(labelNombre);
+        add(labelApellido);
+        add(campoNombre);
+        add(campoApellido);
+        add(campoND);
+        add(tipoDocumento);
+
+
+    }
+
+    public TextField getCampoApellido() {
+        return campoApellido;
+    }
+
+    public void setCampoApellido(TextField campoApellido) {
+        this.campoApellido = campoApellido;
+    }
+
+    public TextField getCampoNombre() {
+        return campoNombre;
+    }
+
+    public void setCampoNombre(TextField campoNombre) {
+        this.campoNombre = campoNombre;
+    }
+
+    public JComboBox getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(JComboBox tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public TextField getCampoND() {
+        return campoND;
+    }
+
+    public void setCampoND(TextField campoND) {
+        this.campoND = campoND;
+    }
 }
--- PooProyecto/src/MainFrame.java
+++ PooProyecto/src/MainFrame.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
 
-public class MainFrame {
+public class MainFrame extends JFrame {
+private  Formulario formulario;
+    public MainFrame(){
+        setTitle("Interfaz Usuario");
+        setSize(300,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+        add(TopPanel(),BorderLayout.NORTH);
+        formulario = new Formulario();
+        add(formulario);
+        formulario.setVisible(true);
+    }
+
+    private JPanel TopPanel(){
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new FlowLayout());
+        JLabel lb1 = new JLabel("Sistema de Gestion");
+        panel1.setBackground(Color.GREEN);
+        panel1.add(lb1);
+        return panel1;
+
+    }
+
+
+
+
 }
